46f891a8ff06c34e5cdd09f195a88a6e573e9805
kimhant02 <khimhan23@gmail.com>
2024-07-15 11:46:26 +0700

    some msgupdate

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#include "module.h"
+
+
 
 // put function declarations here:
 int myFunction(int, int);
---
+++ src/module.h
@@ -0,0 +1,13 @@
+#ifndef module_h
+#define module_h
+
+#include  <Arduino.h>
+#include <WiFi.h>
+
+extern const char* ssid;
+extern const char* password;
+
+void setup_wifi();
+
+
+#endif
